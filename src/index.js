d4886b1c83250960516b642a675a7a8b63eb1ddd
Brian Butcher <butcher.brian.c@gmail.com>
2020-01-07 17:04:57 -0500

    serve publication list app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-const express = require('express')
+const express = require('express');
+
 const api = express();
+api.use(express.static(__dirname + '/public'));
+
 api.listen(3000, () => {
     console.log('API up and running!');
 });
 
-api.get('/', (req, res) => {
-    console.log(req);
-    res.send('Fuck you, World!');
-});
+// api.get('/', (req, res) => {
+//     console.log(req);
+//     res.send('Fuck you, World!');
+// });
